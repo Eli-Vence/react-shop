8f3a5b41667b72f4613392ec2c1a509825b99005
Brav0 <rybinil1998@gmail.com>
2022-10-06 12:49:37 +0300

    Add link to repo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,17 @@ function Header() {
     <nav className='blue darken-4'>
       <div className='nav-wrapper'>
         <a href='!#' className='brand-logo'>
-          Logo
+          React Shop
         </a>
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
-            <a href='!#'>React Shop</a>
+            <a
+              href='https://github.com/Eli-Vence/react-shop'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
